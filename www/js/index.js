e1f0b8f317e21c44a41b554d23d5d1a4f8b0ea01
Vidya Sagar Sankaranarayanan <vidya.sagar@bs-shipmanagement.com>
2014-04-20 00:53:18 +0530

    devgirl version

--- www/js/index.js
+++ www/js/index.js
@@ -57,27 +57,30 @@ var app = {
 
 function try_reg() {
     alert('trying reg');
-    pushNotification = window.plugins.pushNotification;
-    // if ( device.platform == 'android' || device.platform == 'Android' )
-    // {
-        pushNotification.register(
-            successHandler,
-            errorHandler, {
-                "senderID":"213694031514",
-                "ecb":"onNotificationGCM"
-            });
-    // }
-    // else
-    // {
+    // pushNotification = window.plugins.pushNotification;
+    // // if ( device.platform == 'android' || device.platform == 'Android' )
+    // // {
     //     pushNotification.register(
-    //         tokenHandler,
+    //         successHandler,
     //         errorHandler, {
-    //             "badge":"true",
-    //             "sound":"true",
-    //             "alert":"true",
-    //             "ecb":"onNotificationAPN"
+    //             "senderID":"213694031514",
+    //             "ecb":"onNotificationGCM"
     //         });
-    // }
+    // // }
+    // // else
+    // // {
+    // //     pushNotification.register(
+    // //         tokenHandler,
+    // //         errorHandler, {
+    // //             "badge":"true",
+    // //             "sound":"true",
+    // //             "alert":"true",
+    // //             "ecb":"onNotificationAPN"
+    // //         });
+    // // }
+
+    var pushNotification = window.plugins.pushNotification;
+    pushNotification.register(successHandler, errorHandler,{"senderID":"213694031514","ecb":"onNotificationGCM"});
 }
 // result contains any message sent from the plugin call
 function successHandler (result) {
@@ -116,54 +119,27 @@ function onNotificationAPN (event) {
 
 // Android
 function onNotificationGCM(e) {
-    $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
-
     switch( e.event )
     {
-    case 'registered':
-        if ( e.regid.length > 0 )
-        {
-            $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
-            // Your GCM push server needs to know the regID before it can push to this device
-            // here is where you might want to send it the regID for later use.
-            console.log("regID = " + e.regid);
-            alert(e.regid);
-        }
-    break;
-
-    case 'message':
-        // if this flag is set, this notification happened while we were in the foreground.
-        // you might want to play a sound to get the user's attention, throw up a dialog, etc.
-        if ( e.foreground )
-        {
-            $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
-
-            // if the notification contains a soundname, play it.
-            var my_media = new Media("/android_asset/www/"+e.soundname);
-            my_media.play();
-        }
-        else
-        {  // otherwise we were launched because the user touched a notification in the notification tray.
-            if ( e.coldstart )
+        case 'registered':
+            if ( e.regid.length > 0 )
             {
-                $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+                console.log("Regid " + e.regid);
+                alert('registration id = '+e.regid);
             }
-            else
-            {
-                $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
-            }
-        }
+        break;
 
-        $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-        $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
-    break;
+        case 'message':
+          // this is the actual push notification. its format depends on the data model from the push server
+          alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+        break;
 
-    case 'error':
-        $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
-    break;
+        case 'error':
+          alert('GCM error = '+e.msg);
+        break;
 
-    default:
-        $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
-    break;
-  }
+        default:
+          alert('An unknown GCM event has occurred');
+          break;
+    }
 }
