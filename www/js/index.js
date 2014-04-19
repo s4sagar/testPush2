c33069fe837c987a651e0412698b803696469990
Vidya Sagar Sankaranarayanan <vidya.sagar@bs-shipmanagement.com>
2014-04-19 23:51:23 +0530

    Add push ref

--- www/js/index.js
+++ www/js/index.js
@@ -35,7 +35,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        // app.receivedEvent('deviceready');
+        app.receivedEvent('deviceready');
         pushNotification = window.plugins.pushNotification;
         if ( device.platform == 'android' || device.platform == 'Android' )
         {
